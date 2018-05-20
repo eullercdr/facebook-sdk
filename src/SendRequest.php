900b9cb7607e87db07c8952a147c0bf8ff88eccc
Euller Cristian <eullercdr@gmail.com>
2018-05-20 16:57:31 -0300

    Classe de leitura de entrada de dados

---
+++ src/SendRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace CodeBot;
+
+
+class SendRequest
+{
+    private $event;
+
+    /**
+     * SendRequest constructor.
+     * @param $event
+     */
+    public function __construct()
+    {
+        $event = file_get_contents("php://input");
+        $event = json_decode($event, true, 512, JSON_BIGINT_AS_STRING);
+        $this->event = $event['entry'][0]['messaging'][0];
+    }
+
+    public function getSendId()
+    {
+        return $this->event['sender']['id'] ?? null;
+    }
+
+    public function getMessage()
+    {
+        return $this->event['message']['text'] ?? null;
+    }
+
+    public function getPostback()
+    {
+
+        if (empty($this->event['postback'])) {
+            return null;
+        }
+
+        if (is_array($this->event['postback']) and !empty($this->event['postback']['payload'])) {
+            return $this->event['postback']['payload'];
+        }
+
+        return $this->event['postback'] ?? null;
+    }
+
+
+}
