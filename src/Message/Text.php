8e69420f625bac48513a732bf3a32e282c6c13ff
Euller Cristian <eullercdr@gmail.com>
2018-05-20 17:26:53 -0300

    Correção parametro classe Text

--- src/Message/Text.php
+++ src/Message/Text.php
@@ -22,7 +22,7 @@ class Text implements Message
                 'id' => $this->recipientId
             ],
             'message' => [
-                'text' => 'oi',
+                'text' => $messageText,
                 'metadata' => 'DEVELOPER_DEFINED_METADATA'
             ]
         ];
